911df8ff4110c59172c9b1179ec277bc8c4cd47e
André Avancini <andreavancini@alunos.utfpr.edu.br>
2023-06-04 19:53:11 -0300

    pool connection

--- db/conn.js
+++ db/conn.js
@@ -6,11 +6,13 @@ const dbPass = process.env.DB_PASS;
 
 async function main() {
     try {
+        // maxPoolSize -> Pool de conexões 
+        // socketTimeoutMS -> Fecha a conexão após 45 segundos de inatividade
         await mongoose.connect(
-            `mongodb+srv://${dbUser}:${dbPass}@cluster0.wpemlmw.mongodb.net/test`
+            `mongodb+srv://${dbUser}:${dbPass}@cluster0.wpemlmw.mongodb.net/test`, { maxPoolSize: 5, socketTimeoutMS: 45000 }
         )
-        
-        console.log(" ======== CONECTADO AO BD ======== ")
+
+        console.log(" ======== CONECTADO AO BD ======== ");
     } catch (error) {
         console.log(`Erro: ${error}`);
     }
